e47fec9e590d5f7909b1e59a6cebe5347f3fb67c
Marco-Silvera <marksilver_1_0@hotmail.com>
2024-03-03 00:39:52 -0500

    fallos con el encriptador fixeados

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const header = document.querySelector(".ningun")
 let btnCopy = document.querySelector(".copiar")
 let texto = document.getElementById("texto-copiado")
 
+const matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]
+
 function btnEncriptar() {
     if (textArea.value == "") {
         return
@@ -29,39 +31,37 @@ function btnDesencriptar() {
         const textoDesencriptado = desencriptar(textArea.value)
         mensaje.value = textoDesencriptado
         textArea.value = ""
+
+        mensaje.style.display = "flex"
+        btnCopy.style.display = "block"
+        img.style.display = "none"
+        header.style.display = "none"
+        inst.style.display = "none";
     }
 }
 
 function encriptar(stringEncriptado) {
-
-    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]
-
-    stringEncriptado = stringEncriptado.toLowerCase()
-
     for (let i = 0; i < matrizCodigo.length; i++) {
         if (stringEncriptado.includes(matrizCodigo[i][0])) {
-            // Sustituye el valor de indice [i][0] por [i][1]
-            stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+            stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
         }
     }
     return stringEncriptado
-
 }
 
+// const matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]
+
 function desencriptar(stringDesencriptado) {
-
-    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]
-
-    stringDesencriptado = stringDesencriptado.toLowerCase()
-
-    for (let i = 0; i < matrizCodigo.length; i++) {
-        if (stringDesencriptado.includes(matrizCodigo[i][0])) {
-            // Sustituye el valor de indice [i][1] por [i][0]
-            stringDesencriptado = stringDesencriptado.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+    for (let i = 0; i < stringDesencriptado.length; i++) {
+        for (let j = 0; j < matrizCodigo.length; j++) {
+            const codigo = matrizCodigo[j];
+            if (stringDesencriptado.substring(i).startsWith(codigo[1])) {
+                stringDesencriptado = stringDesencriptado.substring(0, i) + codigo[0] + stringDesencriptado.substring(i + codigo[1].length);
+                break; // Salir del bucle interno una vez que se haya encontrado y reemplazado el código
+            }
         }
     }
-    return stringDesencriptado
-
+    return stringDesencriptado;
 }
 
 async function copy() {
