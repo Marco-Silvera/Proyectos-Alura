780e6b67c2ea22ce164eb2173148b04c092d48da
Marco-Silvera <marksilver_1_0@hotmail.com>
2024-04-08 00:16:37 -0500

    Sección sobre mi y skills

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Raleway': ['Raleway', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
